303198d0302270a56447bcae275e59568ba1b582
Facundo Moreno <facumoreno2001@gmail.com>
2026-03-06 22:34:30 -0300

    Codex 8.0

--- front/Andres_Bakery/eslint.config.js
+++ front/Andres_Bakery/eslint.config.js
@@ -5,15 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores([
-    'dist',
-    'src/auth/**',
-    'src/components/**',
-    'src/pages/**',
-    'src/routes.js',
-    'src/styles.css',
-    'src/App.css',
-  ]),
+  globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
